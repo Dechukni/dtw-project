1d24eb365366979b4ce2d27a1c18dee42d1716ab
Yana Halas <yngalas@gmail.com>
2020-01-12 00:16:00 +0200

    display algorithms results to the table

--- src/app/edit-distance/edit-distance.component.ts
+++ src/app/edit-distance/edit-distance.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+
+import { EditDistance } from '../algorithms/edit-distance';
 
 @Component({
   selector: 'app-edit-distance',
@@ -9,7 +11,15 @@ import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 })
 export class EditDistanceComponent implements OnInit {
   form: FormGroup;
-  constructor(private readonly fb: FormBuilder) { }
+  firstStringItems: Array<string>;
+  secondStringItems: Array<string>;
+  resultMatrix: Array<number[]>;
+  distance: number;
+  hoverRow: number;
+  hoverColumn: number
+
+  constructor(private readonly fb: FormBuilder,
+              private readonly cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.form = this.buildForm();
@@ -17,12 +27,31 @@ export class EditDistanceComponent implements OnInit {
 
   buildForm() {
     return this.fb.group({
-      firstString: ['', [Validators.required, Validators.pattern('^\\S*$')]],
-      secondString: ['', [Validators.required, Validators.pattern('^\\S*$')]]
+      firstString: ['abracadabra', [Validators.required, Validators.pattern('^\\S*$')]],
+      secondString: ['abarcababbra', [Validators.required, Validators.pattern('^\\S*$')]]
     });
   }
 
   runEditDistance() {
-    console.log('Run EditDistance', this.form.value);
+    if (this.form.valid) {
+      this.firstStringItems = this.formValueToArray(this.form.value.firstString);
+      this.secondStringItems = this.formValueToArray(this.form.value.secondString);
+
+      const { matrix, distance } = EditDistance.run(this.firstStringItems, this.secondStringItems);
+
+      this.resultMatrix = matrix;
+      this.distance = distance;
+
+      this.cdr.markForCheck();
+    }
+  }
+
+  onMouseOver(row: number, column: number) {
+    this.hoverRow = row;
+    this.hoverColumn = column;
+  }
+
+  private formValueToArray(value: string): Array<string> {
+    return value.split('');
   }
 }
